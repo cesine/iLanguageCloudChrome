e435ec72579d7c8b15bf9f6b5b5893be4c5dba4c
cesine <cesine@yahoo.com>
2014-05-22 15:04:27 +0400

    using chrome storage
    for
    https://github.com/iLanguage/iLanguageCloud/issues/22

--- app/app/scripts/controllers/todoCtrl.js
+++ app/app/scripts/controllers/todoCtrl.js
@@ -7,10 +7,28 @@
  * - exposes the model to the template and provides event handlers
  */
 todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $filter, todoStorage) {
-  var todos = $scope.todos = todoStorage.get();
+  var todos = $scope.todos = [];
+
+  // If there is saved data in storage, use it.
+  // https://developer.chrome.com/apps/app_codelab5_data
+  $scope.load = function(value) {
+    if (value) {
+      todos = $scope.todos = value;
+      $scope.remainingCount = $filter('filter')(todos, {
+        completed: false
+      }).length;
+    }
+  }
+
+  todoStorage.get(function(todos) {
+    console.log("Got some todos", todos);
+    $scope.$apply(function() {
+      $scope.load(todos);
+    });
+  });
 
   $scope.newTodo = '';
-  $scope.remainingCount = $filter('filter')(todos, {completed: false}).length;
+  $scope.remainingCount = 0;
   $scope.editedTodo = null;
 
   if ($location.path() === '') {
@@ -19,15 +37,22 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $filter, tod
 
   $scope.location = $location;
 
-  $scope.$watch('location.path()', function (path) {
-    $scope.statusFilter = { '/active': {completed: false}, '/completed': {completed: true} }[path];
+  $scope.$watch('location.path()', function(path) {
+    $scope.statusFilter = {
+      '/active': {
+        completed: false
+      },
+      '/completed': {
+        completed: true
+      }
+    }[path];
   });
 
-  $scope.$watch('remainingCount == 0', function (val) {
+  $scope.$watch('remainingCount == 0', function(val) {
     $scope.allChecked = val;
   });
 
-  $scope.addTodo = function () {
+  $scope.addTodo = function() {
     var newTodo = $scope.newTodo.trim();
     if (newTodo.length === 0) {
       return;
@@ -43,13 +68,13 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $filter, tod
     $scope.remainingCount++;
   };
 
-  $scope.editTodo = function (todo) {
+  $scope.editTodo = function(todo) {
     $scope.editedTodo = todo;
     // Clone the original todo to restore it on demand.
     $scope.originalTodo = angular.extend({}, todo);
   };
 
-  $scope.doneEditing = function (todo) {
+  $scope.doneEditing = function(todo) {
     $scope.editedTodo = null;
     todo.title = todo.title.trim();
 
@@ -60,31 +85,31 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, $filter, tod
     todoStorage.put(todos);
   };
 
-  $scope.revertEditing = function (todo) {
+  $scope.revertEditing = function(todo) {
     todos[todos.indexOf(todo)] = $scope.originalTodo;
     $scope.doneEditing($scope.originalTodo);
   };
 
-  $scope.removeTodo = function (todo) {
+  $scope.removeTodo = function(todo) {
     $scope.remainingCount -= todo.completed ? 0 : 1;
     todos.splice(todos.indexOf(todo), 1);
     todoStorage.put(todos);
   };
 
-  $scope.todoCompleted = function (todo) {
+  $scope.todoCompleted = function(todo) {
     $scope.remainingCount += todo.completed ? -1 : 1;
     todoStorage.put(todos);
   };
 
-  $scope.clearCompletedTodos = function () {
-    $scope.todos = todos = todos.filter(function (val) {
+  $scope.clearCompletedTodos = function() {
+    $scope.todos = todos = todos.filter(function(val) {
       return !val.completed;
     });
     todoStorage.put(todos);
   };
 
-  $scope.markAll = function (completed) {
-    todos.forEach(function (todo) {
+  $scope.markAll = function(completed) {
+    todos.forEach(function(todo) {
       todo.completed = !completed;
     });
     $scope.remainingCount = completed ? todos.length : 0;
--- app/app/scripts/services/todoStorage.js
+++ app/app/scripts/services/todoStorage.js
@@ -2,18 +2,38 @@
 'use strict';
 
 /**
- * Services that persists and retrieves TODOs from localStorage
-*/
-todomvc.factory('todoStorage', function () {
-	var STORAGE_ID = 'todos-angularjs-perf';
+ * Services that persists and retrieves TODOs handle when the chrome storage changes from another user
+ * https://developer.chrome.com/apps/app_codelab5_data
+ */
+todomvc.factory('todoStorage', function() {
+  var STORAGE_ID = 'todos-angularjs-perf';
 
-	return {
-		get: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-		},
+  return {
+    get: function(callback) {
+      chrome.storage.sync.get(STORAGE_ID,
+        function(data) {
 
-		put: function (todos) {
-			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-		}
-	};
+          var storageHolder = [];
+          if (chrome.runtime.lastError || !data || !data[STORAGE_ID]) {
+            console.log("got no todos", data);
+            /* error */
+          } else {
+            storageHolder = data[STORAGE_ID];
+            console.log("got some todos", storageHolder);
+          }
+          if (typeof callback == "function") {
+            callback(storageHolder);
+          }
+        });
+    },
+
+    put: function(todos) {
+      var todoList = JSON.stringify(todos);
+      console.log("setting todos", todoList);
+
+      chrome.storage.sync.set({
+        'todos-angularjs-perf': todos
+      });
+    }
+  };
 });
