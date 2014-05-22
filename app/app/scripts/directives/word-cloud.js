9b9012bf4367c99cd06b9ce62945e7a7f619b4b7
cesine <cesine@yahoo.com>
2014-05-22 17:15:30 +0400

    moved word cloud html into directive

--- app/app/scripts/directives/word-cloud.js
+++ app/app/scripts/directives/word-cloud.js
@@ -1,12 +1,11 @@
 'use strict';
 
-angular.module('WordCloudApp')
-  .directive('wordCloud', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the wordCloud directive');
-      }
-    };
-  });
+angular.module('WordCloudApp').directive('wordCloud', function() {
+  return {
+    templateUrl: 'views/word-cloud.html',
+    restrict: 'A',
+    link: function postLink() {
+      // element.text('this is the wordCloud directive');
+    }
+  };
+});
