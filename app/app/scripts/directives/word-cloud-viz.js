fbcce33763e8850ef2065deb2ab2257be972f8a0
cesine <cesine@yahoo.com>
2014-05-25 18:15:15 +0400

    adding more fields for more transparency with the underlying process

--- app/app/scripts/directives/word-cloud-viz.js
+++ app/app/scripts/directives/word-cloud-viz.js
@@ -7,54 +7,93 @@ angular.module('WordCloudApp').directive('wordCloudViz', function() {
     template: '<div></div>',
     restrict: 'A',
     controller: function($scope, $element) {
-      var cloud = {
-        text: '',
-        isAndroid: false,
-        font: '',
-        element: [],
-        height: 200,
-        stopWords: /^(და|აის|კასატორი|არ|მე|მიერ|თუ|არა|ფი|ეს|არის|მის|ან)$/
-        // |სა-, სტა-,იმის,-ში/
-      };
-      console.log('in controller wordCloudViz');
-      $scope.wordCloud.font = 'FreeSerif';
-
-      cloud.text = $scope.wordCloud.text;
-      /* dont make clouds of short texts */
-      if (!cloud.text || cloud.text.length < 20) {
+      /* don't make clouds of short texts */
+      if (!$scope.wordCloud.text || $scope.wordCloud.text.length < 20) {
         return;
       }
+      console.log('Setting up wordCloudViz controller with a word cloud from scope');
 
-      /* make the longer texts have more space */
-      if (cloud.text && cloud.text.length > 300) {
+      /* Add values from the database */
+      var cloud = {
+        text: $scope.wordCloud.text,
+        title: $scope.wordCloud.title,
+        isAndroid: false,
+        font: $scope.wordCloud.font || 'FreeSerif',
+        element: $element[0],
+        height: 200,
+        // stopWordsArray: ["და", "აის", "კასატორი", "არ", "მე", "მიერ", "თუ", "არა", "ფი", "ეს", "არის", "მის", "ან"],
+        stopWordsArray: $scope.wordCloud.stopWordsArray || [],
+        prefixesArray: $scope.wordCloud.prefixesArray || [], // |სა-, სტა-,იმის,-ში/
+        suffixesArray: $scope.wordCloud.suffixesArray || [],
+        punctuationArray: $scope.wordCloud.punctuationArray || [],
+        wordFrequencies: $scope.wordCloud.wordFrequencies || [],
+        lexicalExperience: $scope.wordCloud.lexicalExperience || {}
+      };
+
+      /* make the longer texts have more vertical space */
+      if ($scope.wordCloud.text && $scope.wordCloud.text.length > 300) {
         cloud.height = 400;
       } else {
         cloud.height = 200;
       }
 
-      if (!$scope.wordCloud.title && $scope.wordCloud.text) {
-        var titleLength = $scope.wordCloud.text.length > 31 ? 30 : $scope.wordCloud.text.length - 1;
-        $scope.wordCloud.title = $scope.wordCloud.text.substring(0, titleLength) + '...';
+      /* Create a title if not present */
+      if (!cloud.title && cloud.text) {
+        var titleLength = cloud.text.length > 31 ? 30 : cloud.text.length - 1;
+        cloud.title = cloud.text.substring(0, titleLength) + '...';
       }
-      // $scope.wordCloud.stopWords = /^(like|feel|soul|around|the|and|that|my|i|of|was|so|with|a|in|when|then|which|these|us|as|me|an|am|at|be|is|by)$/;
-      $scope.wordCloud.stopWordsSpaceSeparated = $scope.wordCloud.stopWordsSpaceSeparated || '';
-      $scope.wordCloud.stopWordsArray = $scope.wordCloud.stopWordsSpaceSeparated.split(' ');
-      $scope.wordCloud.morphemes = $scope.wordCloud.morphemes;
 
-      cloud.element = $element[0];
-      cloud.stopWords = $scope.wordCloud.stopWords;
-      cloud.font = $scope.wordCloud.font;
+      /* create the cloud */
+      cloud = iLanguageCloud(cloud);
 
-      window.opts = cloud;
-      cloud = iLanguageCloud(cloud).render();
-      $scope.wordCloud.stopWordsArray = cloud.stopWordsArray;
-      $scope.wordCloud.stopWordsSpaceSeparated = cloud.stopWordsArray.join(' ');
-      console.log(cloud);
+      $scope.wordCloud.title = cloud.title;
+      $scope.wordCloud.stopWordsSpaceSeparated = cloud.stopWordsArray.join(' ').trim();
+      $scope.wordCloud.morphemesSpaceSeparated = (cloud.prefixesArray.join(' ') + ' ' + cloud.suffixesArray.join(' ')).trim();
+      $scope.wordCloud.punctuationSpaceSeparated = cloud.punctuationArray.join(' ').trim();
+      // $scope.wordCloud.wordFrequenciesLineBreakSeparated = cloud.wordFrequencies.map(function(word) {
+      //   return word.orthography + ' ' + word.count;
+      // }).join('\n');
+      // $scope.wordCloud.lexicalExperienceJSON = JSON.stringify(cloud.lexicalExperience);
 
-      // $scope.$watch($scope.wordCloud.title, function(newValue, oldValue) {
-      //   cloud.text = newValue.text;
-      //   cloud.render();
-      // });
+      /* when to re-generate the morpheme segmented text */
+      $scope.$watch('wordCloud.text', function(newValue, oldValue) {
+        cloud.runSegmenter();
+        $scope.wordCloud.segmentedText = cloud.segmentedText;
+      });
+      $scope.$watch('wordCloud.morphemesSpaceSeparated', function(newValue, oldValue) {
+        cloud.runSegmenter();
+        $scope.wordCloud.segmentedText = cloud.segmentedText;
+      });
+
+      /* when to re-generate the lexical experience */
+      $scope.$watch('wordCloud.segmentedText', function(newValue, oldValue) {
+        cloud.runWordFrequencyGenerator();
+        $scope.wordCloud.lexicalExperienceJSON = JSON.stringify(cloud.lexicalExperience);
+      });
+      $scope.$watch('wordCloud.punctuationSpaceSeparated', function(newValue, oldValue) {
+        cloud.runWordFrequencyGenerator();
+        $scope.wordCloud.lexicalExperienceJSON = JSON.stringify(cloud.lexicalExperience);
+      });
+
+      /* when to re-generate the frequency list */
+      $scope.$watch('wordCloud.stopWordsSpaceSeparated', function(newValue, oldValue) {
+        cloud.runStemmer();
+        $scope.wordCloud.wordFrequenciesLineBreakSeparated = cloud.wordFrequencies.map(function(word) {
+          return word.orthography + ' ' + word.count;
+        }).join('\n');
+      });
+      $scope.$watch('wordCloud.lexicalExperienceJSON', function(newValue, oldValue) {
+        cloud.runStemmer();
+        $scope.wordCloud.wordFrequenciesLineBreakSeparated = cloud.wordFrequencies.map(function(word) {
+          return word.orthography + ' ' + word.count;
+        }).join('\n');
+      });
+
+      /* when to re-generate the word cloud visualization */
+      $scope.$watch('wordCloud.wordFrequenciesLineBreakSeparated', function(newValue, oldValue) {
+        cloud.render();
+      });
+
     },
     link: function postLink() {
       // element.text('this is the wordCloudViz directive');
