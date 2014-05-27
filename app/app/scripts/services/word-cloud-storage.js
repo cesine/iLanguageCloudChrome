4e5054d2b71a5a9d0d2edc4f93c2f5ad8908e952
cesine <cesine@yahoo.com>
2014-05-27 21:27:53 +0400

    unsuccesful use of chrome storage to store the clouds (too big clouds are never saved)

--- app/app/scripts/services/word-cloud-storage.js
+++ app/app/scripts/services/word-cloud-storage.js
@@ -6,33 +6,82 @@
  * https://developer.chrome.com/apps/app_codelab5_data
  */
 WordCloudApp.factory('wordCloudStorage', function() {
-  var STORAGE_ID = 'wordClouds-angularjs-perf';
+  var STORAGE_ID = 'wordClouds-index';
 
   return {
-    get: function(callback) {
-      chrome.storage.sync.get(STORAGE_ID,
-        function(data) {
+    get: function(callbackForGetIndex) {
+      var self = this;
+      chrome.storage.sync.get(STORAGE_ID, function(data) {
 
-          var storageHolder = [];
-          if (chrome.runtime.lastError || !data || !data[STORAGE_ID]) {
-            console.log('got no wordClouds', data);
-            /* error */
-          } else {
-            storageHolder = data[STORAGE_ID];
-            console.log('got some wordClouds', storageHolder);
+        var storageHolder = [];
+        if (chrome.runtime.lastError || !data || !data[STORAGE_ID]) {
+          console.log('got no wordClouds', data);
+          /* error */
+        } else {
+          storageHolder = JSON.parse(data[STORAGE_ID]);
+          console.log('got some wordClouds', storageHolder);
+          for (var cloudIndex = 0; cloudIndex < storageHolder.length; cloudIndex++) {
+
+            (function(thisCloud, indexInStorage) {
+              self.getId(thisCloud.id, function(wordCloud) {
+                thisCloud.orthography = wordCloud.orthography || 'Word cloud is missing text.';
+                console.log("Filled ", thisCloud);
+                if (indexInStorage === storageHolder.length - 1) {
+                  if (typeof callbackForGetIndex === 'function') {
+                    callbackForGetIndex(storageHolder);
+                  }
+                } else {
+                  console.log("Not resolving storage yet");
+                }
+              });
+            })(storageHolder[cloudIndex], cloudIndex);
+
           }
-          if (typeof callback === 'function') {
-            callback(storageHolder);
-          }
-        });
+        }
+      });
+    },
+
+    getId: function(id, callbackFromGetId) {
+      console.log("looking for " + id);
+      chrome.storage.sync.get(id, function(data) {
+        var wordCloud = {};
+        if (chrome.runtime.lastError || !data || !data[id]) {
+          console.log('got no wordCloud at this id', data);
+          /* error */
+        } else {
+          wordCloud = JSON.parser(data[id]);
+          console.log('got a wordCloud', wordCloud);
+        }
+        if (typeof callbackFromGetId === 'function') {
+          console.log("Fetched ", wordCloud);
+          callbackFromGetId(wordCloud);
+        } else {
+          console.log("never gets to the callbackFromGetId");
+        }
+      });
     },
 
     put: function(wordClouds) {
-      var wordCloudList = JSON.stringify(wordClouds);
+      var wordCloudIndex = []
+      wordClouds.map(function(wordCloud) {
+        var indexable = {
+          title: wordCloud.title,
+          id: wordCloud.id || Date.now(),
+        }
+        wordCloudIndex.push(indexable);
+        //save this cloud's orthography
+        var id = indexable.id;
+        chrome.storage.sync.set({
+          id: JSON.stringify({
+            orthography: wordCloud.orthography
+          })
+        });
+      });
+      JSON.stringify(wordCloudIndex);
       console.log('setting wordClouds');
-
+      //save the index
       chrome.storage.sync.set({
-        'wordClouds-angularjs-perf': wordClouds
+        'wordClouds-index': JSON.stringify(wordCloudIndex)
       });
     }
   };
