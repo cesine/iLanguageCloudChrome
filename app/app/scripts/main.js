b9f77bb3bd91dd8f758bb07cc9fc6fed35d95a65
cesine <cesine@yahoo.com>
2015-07-15 08:17:35 -0400

    adding catch in case we are not in a chrome app

--- app/app/scripts/main.js
+++ app/app/scripts/main.js
@@ -1,19 +1,23 @@
 'use strict';
 
-// Listens for the app launching then creates the window
-if (chrome && chrome.app && chrome.app.runtime && chrome.app.runtime.onLaunched && chrome.app.runtime.onLaunched.addListener) {
-  chrome.app.runtime.onLaunched.addListener(function() {
-    var width = 600;
-    var height = 400;
+try {
+  // Listens for the app launching then creates the window
+  if (chrome && chrome.app && chrome.app.runtime && chrome.app.runtime.onLaunched && chrome.app.runtime.onLaunched.addListener) {
+    chrome.app.runtime.onLaunched.addListener(function() {
+      var width = 600;
+      var height = 400;
 
-    chrome.app.window.create('index.html', {
-      id: 'main',
-      bounds: {
-        width: width,
-        height: height,
-        left: Math.round((screen.availWidth - width) / 2),
-        top: Math.round((screen.availHeight - height) / 2)
-      }
+      chrome.app.window.create('index.html', {
+        id: 'main',
+        bounds: {
+          width: width,
+          height: height,
+          left: Math.round((screen.availWidth - width) / 2),
+          top: Math.round((screen.availHeight - height) / 2)
+        }
+      });
     });
-  });
+  }
+} catch (exception) {
+  console.log('Caught exception', exception);
 }
